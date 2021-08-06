2cf9804fa6dfcd6112289f5bdc65673454a33a2c
Sean Liu <siang720@gmail.com>
2021-08-06 14:23:06 +0800

    add components

---
+++ src/components/Charts/Charts.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Charts = () => {
+  return <div>
+    <p>Charts</p>
+  </div>
+};
+
+export default Charts;
---
+++ src/components/Selector/Selector.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Selector = () => {
+  return <div>
+    <p>Selector</p>
+  </div>
+};
+
+export default Selector;
