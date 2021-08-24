2371e62082d24027143b7878e3eb60729ef278e2
Sean Liu <siang720@gmail.com>
2021-08-24 16:57:28 +0800

    add dropdown select feature

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Selector from './components/Selector/Selector';
 import Charts from './components/Charts/Charts';
 import chartCollections from './data/chart-collections.json';
+import releases from './data/releases.json';
+import sources from './data/sources.json';
 
 function App() {
-  const charts = chartCollections;
+  const [filteredReleaseId, setFilteredReleaseId] = useState(0);
+  const [filteredSourceId, setFilteredSourceId] = useState(0);
+
+  const filteredCharts = chartCollections.filter(chart => {
+    if (filteredReleaseId === 0 && filteredSourceId === 0) return true
+    if (filteredReleaseId === 0 && filteredSourceId !== 0) {
+      return chart.source_id === filteredSourceId
+    }
+    if (filteredReleaseId !== 0 && filteredSourceId === 0) {
+      return chart.release_id === filteredReleaseId
+    }
+    if (filteredReleaseId !== 0 && filteredSourceId !== 0) {
+      return chart.release_id === filteredReleaseId && chart.source_id === filteredSourceId
+    }
+  });
+
+  const releaseIdChangeHandler = (selectedReleaseId) => {
+    setFilteredReleaseId(selectedReleaseId);
+  };
+
+  const sourceIdChangeHandler = (selectedSourceId) => {
+    setFilteredSourceId(selectedSourceId);
+  };
 
   return (
     <div className="App">
       <Navbar />
-      <Selector />
-      <Charts charts={charts} />
+      <Selector
+        selectedReleaseId={filteredReleaseId}
+        selectedSourceId={filteredSourceId}
+        releases={releases}
+        sources={sources}
+        onReleaseIdChange={releaseIdChangeHandler}
+        onSourceIdChange={sourceIdChangeHandler}
+      />
+      <Charts charts={filteredCharts} />
     </div>
   );
 }
--- src/components/Selector/Selector.js
+++ src/components/Selector/Selector.js
@@ -3,22 +3,38 @@ import styles from './Selector.module.css';
 import Form from 'react-bootstrap/Form';
 import { Row, Col } from 'react-bootstrap';
 
-const Selector = () => {
+const Selector = (props) => {
+  const releaseIdChange = (event) => {
+    props.onReleaseIdChange(Number(event.target.value));
+  };
+
+  const sourceIdChange = (event) => {
+    props.onSourceIdChange(Number(event.target.value));
+  };
+
   return <Row className={styles.selector}>
     <Col sm={12} md={6} lg={4} className={styles.selectorItem}>
-      <Form.Select aria-label="Default select example">
-        <option>Open this select menu</option>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
+      <Form.Select
+        aria-label="Default select example"
+        defaultValue={props.selectedSourceId}
+        onChange={sourceIdChange}
+      >
+        <option value={0}>All Sources</option>
+        {props.sources.map((e) => (
+          <option value={e.id} key={e.id}>{e.name}</option>
+        ))}
       </Form.Select>
     </Col>
-    <Col sm={12} md={6} lg={4}>
-      <Form.Select aria-label="Default select example">
-        <option>Open this select menu</option>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
+    <Col sm={12} md={6} lg={4} className={styles.selectorItem}>
+      <Form.Select
+        aria-label="Default select example"
+        defaultValue={props.selectedReleaseId}
+        onChange={releaseIdChange}
+      >
+        <option value={0}>All Releases</option>
+        {props.releases.map((e) => (
+          <option value={e.id} key={e.id}>{e.name}</option>
+        ))}
       </Form.Select>
     </Col>
   </Row>
